ee0b5e181a9ca551c4786106e52d6a5aa9300a68
awesomealex85 <alexec206@gmail.com>
2021-03-19 17:34:05 -0700

    lesson09 updates

--- checkout.js
+++ checkout.js
@@ -4,28 +4,41 @@ function dogProfile(name, breed, cost) {
   alert("Name: " + name + " Breed: " + breed + " Cost: " + cost);
 }
 
+$(document).ready(function(){
+  $('form').submit(function(event) {
+    event.preventDefault();
+    alert('Thank you. The form information has been received');
+
+    let form = document.querySelector('form');
+    let text = "";
+    let i;
+
+    for (i = 0; i < form.length ; i++) {
+      text += form.elements[i].value + " ";
+    }
+    console.log(text);
+  });
+
 let total = 0;
 
-function toCart(cost) {
-  total = cost + total;
-  alert('Total Cost: $' + total);
-  return total;
-}
-// const form = document.querySelector('form');
+$('p.adopt').click(function(){
+    let dollarAmount = $(this).parent().find('span').text();
+    let cost = parseFloat(dollarAmount).toFixed(2);
+    total = cost + total;
+    alert('Total Cost: $' + total);
+    return total;
+  console.log(cost);
+})
 
-// let formInput = [];
+$(function(){
+  $("li.border").hover(
+        function() { $(this).addClass("Hover"); },
+        function() { $(this).removeClass("Hover"); }
+    );
 
-document.addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Thank you. The form information has been received');
-
-  let form = document.querySelector('form');
-  let text = "";
-  let i;
-  for (i = 0; i < form.length ; i++) {
-    text += form.elements[i].value + " ";
-
-  }
-  console.log(text);
-
+  $('li.border').click(function() {
+    this.toggle = !this.toggle;
+        $(this).stop().fadeTo(400, this.toggle ? 0.4 : 1);
+    })
+  })
 });
